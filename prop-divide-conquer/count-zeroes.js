6df871b72b3799cf1f80f3c3c8a9e684d5e6efe4
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-07 21:16:05 -0800

    Completed Exercises

--- prop-divide-conquer/count-zeroes.js
+++ prop-divide-conquer/count-zeroes.js
@@ -1,5 +1,20 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let zeroCount = 0;
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (middleVal === 0) {
+      if (arr[middleIdx - 1] === 1 || arr[middleIdx - 1] === undefined) {
+        zeroCount = arr.splice(middleIdx).length;
+      }
+      rightIdx = middleIdx - 1;
+    } else {
+      leftIdx = middleIdx + 1;
+    }
+  }
+  return zeroCount;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
--- prop-divide-conquer/find-rotated-index.js
+++ prop-divide-conquer/find-rotated-index.js
@@ -1,5 +1,43 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num) {
+  const pivot = findPivot(arr);
+  if (pivot > 0 && num >= arr[0] && num <= arr[pivot - 1]) {
+    return binarySearch(arr, num, 0, pivot - 1);
+  } else {
+    return binarySearch(arr, num, pivot, arr.length - 1);
+  }
 }
 
-module.exports = findRotatedIndex
+function binarySearch(arr, num, leftIdx = 0, rightIdx = arr.length - 1) {
+  if (arr.length === 0) return -1;
+  if (num < arr[leftIdx] || num > arr[rightIdx]) return -1;
+
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (middleVal < num) {
+      leftIdx = middleIdx + 1;
+    } else if (middleVal > num) {
+      rightIdx = middleIdx - 1;
+    } else {
+      return middleIdx;
+    }
+  }
+  return -1;
+}
+
+function findPivot(arr) {
+  if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+  var start = 0;
+  var end = arr.length - 1;
+  while (start <= end) {
+    var mid = Math.floor((start + end) / 2);
+    if (arr[mid] > arr[mid + 1]) return mid + 1;
+    else if (arr[start] <= arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+}
+
+module.exports = findRotatedIndex;
